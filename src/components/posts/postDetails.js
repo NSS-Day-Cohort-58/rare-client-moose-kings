c8899fdcd889d651d1cb05714a613f847223c3fc
Jonathan Woodard <jvw.226@gmail.com>
2022-11-22 10:32:19 -0500

    displaying tags for each post

--- src/components/posts/postDetails.js
+++ src/components/posts/postDetails.js
@@ -45,6 +45,21 @@ export const PostDetails = () => {
     });
   };
 
+  const renderTagsForPost = () => {
+    return <>
+    <div className="">
+      {
+        post?.tags?.map(tag => {
+          return <>
+          <div>{tag.label}</div>
+          <br></br>
+          </>
+        })
+      }
+      </div>
+    </>
+  }
+
   const displayPostDetailsCard = () => {
     return (
       <>
@@ -153,7 +168,7 @@ export const PostDetails = () => {
             <div className="level">
               <h3 className="title is-5 level-item">Tags</h3>
             </div>
-            <div className="level-item">{post?.tag?.label}</div>
+            {renderTagsForPost()}
           </div>
         </section>
       </>
